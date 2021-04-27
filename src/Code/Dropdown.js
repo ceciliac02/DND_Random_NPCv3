a90a307f81394ef9d6e5cc44e9920ac7aa5ae061
Cecilia Cruz <cruzscecilia@gmail.com>
2021-04-27 12:32:52 -0700

    Finished mockup of labels

--- src/Code/Dropdown.js
+++ src/Code/Dropdown.js
@@ -1,39 +1,48 @@
 import React from 'react';
 import Data from '../races.json';
- 
-const dropdownOptions = () => {
+
+const raceList = () => {
     const data = Data;
-    const length = data.map((num) => <li className="options">{num.name}</li>);
-    return length;
-    
+    const races = data.map((num) => num);
+
+    const list = races.map((name) => <option key={name.id} id={name.id} className="options" onClick={() => {document.getElementById(name.id).classList.toggle("race-active")}}>{name.name}</option>);
+    return list;
 }
 
-function toggleList () {
-    let classPromise = new Promise((resolve, reject) => {
-        const list = document.querySelectorAll(".options");
-        if (list.length > 0) {
-            resolve('Success')
-        }
-        else {
-            reject('Fail')
-        }
-    })
-    classPromise.then((message) => {
-        console.log(document.querySelectorAll(".options"))
-    }).catch((message) => {
-        console.log("oops")
-    })
+const genderList = () => {
+    const genders = ["Any Gender", "Female", "Male", "Nonbinary"];
+    const list = genders.map((gender) => <option className="options">{gender}</option>);
+    return list;
 }
 
+const heightList = () => {
+    const heights = ["All Heights", "Tall", "Average", "Short"];
+    const list = heights.map((height) => <option className="options">{height}</option>);
+    return list;
+}
+
+const behaviorList = () => {
+    const behaviors = ["All Behaviors", "Friendly", "Hostile", "Apathetic"];
+    const list = behaviors.map((beh) => <option className="options">{beh}</option>);
+    return list;
+}
 
 function Menu () {
+    
+
+
+
     return (
         <div className="labelContainer">
-            <ul>
-                <li className="default" onClick={() => {document.getElementById("list").classList.toggle("active")}}>All Races</li>
-                <div id="list">{dropdownOptions()}</div>
-                
-            </ul>
+            <select id="selectedRace" className="category">{raceList()}</select>
+            <select id="selectedGender" className="category">{genderList()}</select>
+            <select id="selectedHeight" className="category">{heightList()}</select>
+            <div id="hair" className="category">Hair</div>
+            <div id="trait" className="category">Trait</div>
+            <div id="quirk" className="category">Quirk</div>
+            <div id="hobby" className="category">Hobby</div>
+            <select id="behavior" className="category">{behaviorList()}</select>
+            <div id="profession" className="category">Profession</div>
         </div>
     )
 }
